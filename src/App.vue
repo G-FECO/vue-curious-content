e72cd9f5144b7f14a25add2e52b6d788bb379882
kwonmory <kwonmory@gmail.com>
2020-10-23 10:42:45 +0900

    Add Created Part

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Test {{ answer }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      answer: 0,
+    };
+  },
+  async created() {
+    console.log('created before');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    this.answer = 100;
+    console.log('created after');
+  },
+  async mounted() {
+    console.log('mounted');
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
+import App from './App.vue';
 
 Vue.config.errorHandler = (err) => {
   console.log(err.message); // "Oops가 나와야한다."
 };
 
 new Vue({
-  template: '<h1>Hello</h1>',
-  async mounted() {
-    console.log('before');
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    console.log('after');
-    throw new Error('Oops');
-  },
+  render: (h) => h(App),
 }).$mount('#app');
